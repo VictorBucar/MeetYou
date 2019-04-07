3f67dadc70d43e51387ee085f22e985a54805cb1
Victor <victorbucar@outlook.com>
2019-04-07 19:42:58 +0100

    AddedUserEntity to context and database

--- MeetYouApp.API/Migrations/20190407183904_AddUserRntity.Designer.cs
+++ MeetYouApp.API/Migrations/20190407183904_AddUserRntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MeetYouApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MeetYouApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190407183904_AddUserRntity")]
+    partial class AddUserRntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
